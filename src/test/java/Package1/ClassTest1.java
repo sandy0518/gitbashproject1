fd2184b165b34b64cd81fc4560c92294094fbdf8
sandy0518 <sai.sandeep.906@gmail.com>
2022-10-20 11:48:43 +0530

    project pushed

---
+++ src/test/java/Package1/ClassTest1.java
@@ -0,0 +1,9 @@
+package Package1;
+
+public class ClassTest1 {
+
+	public static void main(String[] args) {
+		System.out.println("First package");
+	}
+
+}
---
+++ src/test/java/Package1/ClassTest2.java
@@ -0,0 +1,9 @@
+package Package1;
+
+public class ClassTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("Class Test2");
+	}
+
+}
