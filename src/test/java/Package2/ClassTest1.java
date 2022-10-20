6641394d88b4480c79acf7f19861a616fc45f7a5
sandy0518 <sai.sandeep.906@gmail.com>
2022-10-20 11:58:35 +0530

    2nd package added

---
+++ src/test/java/Package2/ClassTest1.java
@@ -0,0 +1,9 @@
+package Package2;
+
+public class ClassTest1 {
+
+	public static void main(String[] args) {
+		System.out.println("First package");
+	}
+
+}
---
+++ src/test/java/Package2/ClassTest2.java
@@ -0,0 +1,9 @@
+package Package2;
+
+public class ClassTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("Class Test2");
+	}
+
+}
